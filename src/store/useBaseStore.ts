f9b6285af0a056554048bf3bc2ce15ef1ff99206
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-18 06:38:23 +0300

    chore: Add useBaseStore for managing base store state

---
+++ src/store/useBaseStore.ts
@@ -0,0 +1,31 @@
+import WebApp from "@twa-dev/sdk";
+import { create } from "zustand";
+
+type BaseStoreState = {
+  tg: typeof WebApp | null;
+  userId: number | null;
+  userName: string | null;
+};
+
+type BaseStoreActions = {
+  setTg: (tg: typeof WebApp) => void;
+  removeTg: () => void;
+  removeUserId: () => void;
+};
+
+export const useBaseStore = create<BaseStoreState & BaseStoreActions>(
+  (set) => ({
+    tg: null,
+    userId: null,
+    userName: null,
+    setTg: (tg: typeof WebApp) =>
+      set(() => ({
+        tg: tg,
+        userId: tg.initDataUnsafe.user?.id,
+        userName: tg.initDataUnsafe.user?.username,
+      })) as void,
+    removeTg: () => set({ tg: null }),
+    removeUserId: () => set({ userId: null }),
+  })
+);
+// Compare this snippet from src/store/useBaseStore.ts:
