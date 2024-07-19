56f0778b0f989ac097336d8859b08a9d440b9ed5
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-19 05:51:10 +0300

    feat: Add Footer component for website navigation

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='w-full flex justify-evenly h-full '>
+            <div className='bg-blue-400 px-6 py-4 rounded-lg'>
+                Home
+            </div>
+            <div className='px-6 py-4 bg-blue-400 rounded-lg'>Earnn</div>
+            <div className='px-6 py-4 bg-blue-400 rounded-lg'>Wallet</div>
+        </footer>
+    )
+}
+
+export default Footer
