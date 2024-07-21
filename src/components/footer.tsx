fb25644e7b4a52c8ad5a974332c3cfbf9077e587
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-21 04:35:47 +0300

    refactor: Update Footer component with motion animation and improved styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
-
+"use client"
+import { AiOutlineHome } from 'react-icons/ai'
+import { LiaCoinsSolid } from 'react-icons/lia'
+import { IoWalletOutline } from "react-icons/io5";
+import { motion } from 'framer-motion'
 const Footer = () => {
     return (
-        <footer className='w-full flex justify-evenly h-full '>
-            <div className='bg-blue-400 px-6 py-4 rounded-lg'>
-                Home
+        <motion.footer
+
+            initial={{ opacity: 0.0, y: -40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+            }}
+
+            className='w-full pb-4 text-white flex justify-evenly  gap-4 z-20'>
+            <div className='bg-zinc-400/50 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 z-10 transition-all w-1/3 px-4 flex flex-col items-center justify-center gap-1 py-3 rounded-lg'>
+                <AiOutlineHome size={24} />
+                <p>
+                    Home
+                </p>
             </div>
-            <div className='px-6 py-4 bg-blue-400 rounded-lg'>Earnn</div>
-            <div className='px-6 py-4 bg-blue-400 rounded-lg'>Wallet</div>
-        </footer>
+            <div className=' bg-zinc-400/50 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 transition-all  z-10 w-1/3 px-4 flex flex-col items-center justify-center gap-1 py-3 rounded-lg'>
+                <LiaCoinsSolid size={24} />
+                <p>
+                    Earn
+                </p>
+            </div>
+            <div className=' bg-zinc-400/50 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 transition-all z-10 w-1/3 px-4 flex flex-col items-center justify-center gap-1 py-3 rounded-lg'>
+                <IoWalletOutline size={24} />
+                <p>
+                    Wallet
+                </p>
+
+            </div>
+        </motion.footer>
     )
 }
 
