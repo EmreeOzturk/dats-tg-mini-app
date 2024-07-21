b9a65984552f97a4cc4b9c2549a58f93f481f18c
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-21 04:35:26 +0300

    chore: Update Telegram bot webhook URL in route.ts

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -6,6 +6,7 @@ import { Bot, InlineKeyboard, webhookCallback } from "grammy";
 
 const token = process.env.TELEGRAM_TOKEN;
 // https://api.telegram.org/bot<telegram_bot_token>/setWebhook?url=https://<your-deployment.vercel>.app/api/bot
+// https://api.telegram.org/bot7312114197:AAF4oFkxi_hhbOStMot3XQ5T21aVIMf2hE4/setWebhook?url=https://unified-solely-lacewing.ngrok-free.app/api/bot
 if (!token)
   throw new Error("TELEGRAM_BOT_TOKEN environment variable not found.");
 
