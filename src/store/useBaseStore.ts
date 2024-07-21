15726375a57a1f98970e2178d0fc2ac4955f667f
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-21 04:36:39 +0300

    refactor: Update useBaseStore to include userPoints and userTotalTimeOfUsingApp

--- src/store/useBaseStore.ts
+++ src/store/useBaseStore.ts
@@ -5,10 +5,14 @@ type BaseStoreState = {
   tg: typeof WebApp | null;
   userId: number | null;
   userName: string | null;
+  userPoints: number | null;
+  userTotalTimeOfUsingApp: number | null;
 };
 
 type BaseStoreActions = {
   setTg: (tg: typeof WebApp) => void;
+  setUserPoints: (points: number) => void;
+  setUserTotalTimeOfUsingApp: (totalTime: number) => void;
   removeTg: () => void;
   removeUserId: () => void;
 };
@@ -18,6 +22,8 @@ export const useBaseStore = create<BaseStoreState & BaseStoreActions>(
     tg: null,
     userId: null,
     userName: null,
+    userPoints: null,
+    userTotalTimeOfUsingApp: null,
     setTg: (tg: typeof WebApp) =>
       set(() => ({
         tg: tg,
@@ -26,5 +32,8 @@ export const useBaseStore = create<BaseStoreState & BaseStoreActions>(
       })) as void,
     removeTg: () => set({ tg: null }),
     removeUserId: () => set({ userId: null }),
+    setUserPoints: (points: number) => set({ userPoints: points }),
+    setUserTotalTimeOfUsingApp: (totalTime: number) =>
+      set({ userTotalTimeOfUsingApp: totalTime }),
   })
 );
