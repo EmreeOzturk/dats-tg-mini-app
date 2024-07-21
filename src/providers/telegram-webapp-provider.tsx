4fb2851afa3ae31bbef37a14c34c37a034451a3b
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-21 04:36:43 +0300

    refactor: Update TelegramWebAppProvider to include userPoints and userTotalTimeOfUsingApp

--- src/providers/telegram-webapp-provider.tsx
+++ src/providers/telegram-webapp-provider.tsx
@@ -1,21 +1,43 @@
 "use client"
 import { PropsWithChildren, useEffect, useState } from "react"
+import { useQuery } from "@tanstack/react-query";
+import { useBaseStore } from "@/store/useBaseStore";
 
 import useTelegram from "@/hooks/useTelegram";
+import WebApp from "@twa-dev/sdk";
 const TelegramWebAppProvider = ({ children }: PropsWithChildren<{}>) => {
-    const { removeTelegram, initTelegram, telegram } = useTelegram();
-    const [loading, setLoading] = useState(true);
+    const { removeTelegram, initTelegram } = useTelegram();
+    const setUserPoints = useBaseStore((state) => state.setUserPoints);
+    const setUserTotalTimeOfUsingApp = useBaseStore((state) => state.setUserTotalTimeOfUsingApp);
     useEffect(() => {
         initTelegram();
-        setLoading(false);
         return () => {
             removeTelegram();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    return <>
-        {loading ? <div>Loading…</div> : children}
-    </>
-}
 
+    const { data, isLoading } = useQuery({
+        queryFn: async () => await fetch("/api/validateUser", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ telegramInitData: typeof window !== "undefined" && WebApp.initData }),
+        }).then((res) => res.json()).then((data) => {
+            setUserPoints(data.points);
+            setUserTotalTimeOfUsingApp(data.totalTimeOfUsingApp);
+        }),
+        queryKey: ["handleAndValidateUser"],
+    });
+
+
+    return (
+        <>
+            {
+                isLoading ? <div>Loading... </div> : children
+            }
+        </>
+    )
+}
 export default TelegramWebAppProvider
