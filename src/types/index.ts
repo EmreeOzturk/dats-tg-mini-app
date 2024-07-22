4f370d93a1e08af536caccff7070a401cb899d3e
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-22 04:44:42 +0300

    refactor: Update types/index.ts to exclude unnecessary fields from User type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,17 @@
 import { Prisma } from "@prisma/client";
 
-export type User = Omit<Prisma.UserCreateInput,
-    'id' | 'createdAt' | 'updatedAt' | 'completedMissions'
+export type User = Omit<
+  Prisma.UserCreateInput,
+  "id" | "createdAt" | "updatedAt" | "completedMissions"
 >;
+
+export type ConnectionResponseData = {
+  downloadSpeed: number;
+  uploadSpeed: number;
+  downloaded: number;
+  uploaded: number;
+  latency: number;
+  bufferBloat: number;
+  userLocation: string;
+  userIp: string;
+};
