4d8bf1d6f38e61c1b2108faff96f4c4c5d08a3b3
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-22 04:44:36 +0300

    refactor: Update ReactQueryProvider to use ReactQueryClientProvider for managing API data caching and fetching

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,14 +1,20 @@
-"use client";
+'use client'
 
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react'
 
-const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-    const [queryClient] = useState(() => new QueryClient());
-
-    return (
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    );
-};
-
-export default ReactQueryProvider;
+export const ReactQueryClientProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  )
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
