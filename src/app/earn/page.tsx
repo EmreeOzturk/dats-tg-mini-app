ac49bece557f73039dcdd118f033e9c76f35507e
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-29 16:32:49 +0300

    refactor: Create EarnPage component for displaying earn information and task list

---
+++ src/app/earn/page.tsx
@@ -0,0 +1,13 @@
+import EarnInfoSection from "@/components/earn/earn-info"
+import TaskList from "@/components/earn/task-list"
+
+const EarnPage = () => {
+  return (
+    <div className="h-full  flex flex-col items-center justify-start w-full px-8">
+      <EarnInfoSection />
+      <TaskList />
+    </div>
+  )
+}
+
+export default EarnPage
