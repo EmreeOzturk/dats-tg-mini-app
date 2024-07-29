645a97f5ae5a5f6f0cf2af4c6026d338e371913d
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-29 16:33:48 +0300

    feat: Create WalletPage component for displaying wallet information

---
+++ src/app/wallet/page.tsx
@@ -0,0 +1,13 @@
+
+const WalletPage = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-between">
+      <div>
+        Wallet Page
+      </div>
+
+    </div>
+  )
+}
+
+export default WalletPage
