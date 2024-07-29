1850dbec7f37f63a360db5c4d3acb14bef665751
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-29 16:33:51 +0300

    refactor: Update Footer component to use Next.js Link for navigation

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,11 @@
 import { AiOutlineHome } from 'react-icons/ai'
 import { LiaCoinsSolid } from 'react-icons/lia'
 import { IoWalletOutline } from "react-icons/io5";
+import { usePathname } from 'next/navigation';
 import { motion } from 'framer-motion'
+import Link from 'next/link';
 const Footer = () => {
+    const pathname = usePathname()
     return (
         <motion.footer
 
@@ -15,26 +18,31 @@ const Footer = () => {
                 ease: "easeInOut",
             }}
 
-            className='w-full pb-4 text-white flex justify-evenly  gap-4 z-20'>
-            <div className='bg-zinc-400/50 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 z-10 transition-all w-1/3 px-4 flex flex-col items-center justify-center gap-1 py-3 rounded-lg'>
-                <AiOutlineHome size={24} />
-                <p>
-                    Home
-                </p>
-            </div>
-            <div className=' bg-zinc-400/50 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 transition-all  z-10 w-1/3 px-4 flex flex-col items-center justify-center gap-1 py-3 rounded-lg'>
+            className='w-full pb-4 text-blue-950 flex justify-evenly z-10'>
+            <Link
+                href='/earn'
+                className={`bg-zinc-300 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 z-10 transition-all w-1/4 px-4 flex flex-col items-center justify-center  py-3 rounded-lg ${pathname === "/earn" ? 'shadow-lg shadow-sky-500/60 scale-105 text-sky-500 hover:bg-zinc-300 hover:text-sky-500' : ''}`}>
                 <LiaCoinsSolid size={24} />
                 <p>
                     Earn
                 </p>
-            </div>
-            <div className=' bg-zinc-400/50 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 transition-all z-10 w-1/3 px-4 flex flex-col items-center justify-center gap-1 py-3 rounded-lg'>
+            </Link>
+            <Link
+                href='/'
+                className={`bg-zinc-300 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 z-10 transition-all w-1/4 px-4 flex flex-col items-center justify-center py-3 rounded-lg -translate-y-2 ${pathname === "/" ? 'shadow-lg shadow-sky-500/60 scale-105  text-sky-500 hover:bg-zinc-300 hover:text-sky-500' : ''}`}>
+                <AiOutlineHome size={24} />
+                <p >
+                    Home
+                </p>
+            </Link>
+            <Link
+                href='/wallet'
+                className={`bg-zinc-300 cursor-pointer hover:text-zinc-300 hover:bg-zinc-500/80 z-10 transition-all w-1/4 px-4 flex flex-col items-center justify-center  py-3 rounded-lg ${pathname === "/wallet" ? 'shadow-lg shadow-sky-500/60 scale-105 text-sky-500 hover:bg-zinc-300 hover:text-sky-500' : ''}`}>
                 <IoWalletOutline size={24} />
                 <p>
                     Wallet
                 </p>
-
-            </div>
+            </Link>
         </motion.footer>
     )
 }
