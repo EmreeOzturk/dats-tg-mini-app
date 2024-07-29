4e59b323f223b4cf4e9ad1a950518be3f24ea1d0
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-29 16:33:19 +0300

    refactor: Update Header component to use useBaseStore for userName and userPoints

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
 "use client"
 import useTelegram from "@/hooks/useTelegram"
+import { useBaseStore } from "@/store/useBaseStore";
 const Header = () => {
-    const { userName, userPoints } = useTelegram();
+    const userName = useBaseStore((state) => state.userName);
+    const userPoints = useBaseStore((state) => state.userPoints);
     return (
-        <header className='flex w-full overflow-hidden items-center justify-between pt-2 pb-4'>
-            <div className='border flex items-center justify-center gap-2 rounded-full text-sm font-bold border-black px-6 py-2'>
+        <header className='flex w-full overflow-hidden items-center justify-between pt-4 pb-4 '>
+            <div className='border flex items-center justify-center gap-2  text-sm font-bold border-black px-6 py-2'>
                 <p>
                     {userName}
                 </p>
@@ -12,9 +14,9 @@ const Header = () => {
                     XP: {userPoints}
                 </p>
             </div>
-            <button className="bg-black border z-10 tracking-wide border-black text-sm  rounded-full w-fit text-white px-5 py-2">
+            {/* <button className="bg-black border z-10 tracking-wide border-black text-sm  w-fit text-white px-5 py-2">
                 Close App
-            </button>
+            </button> */}
         </header>
     )
 }
