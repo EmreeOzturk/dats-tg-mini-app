e74f77d7762ed4995f3a307c2c95faa244cc0326
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-29 16:34:14 +0300

    refactor: Add userLastCheckInDate and telegramId to useBaseStore

--- src/store/useBaseStore.ts
+++ src/store/useBaseStore.ts
@@ -3,18 +3,23 @@ import { create } from "zustand";
 
 type BaseStoreState = {
   tg: typeof WebApp | null;
+  telegramId: number | null;
   userId: number | null;
   userName: string | null;
   userPoints: number | null;
   userTotalTimeOfUsingApp: number | null;
+  userLastCheckInDate: Date | null;
 };
 
 type BaseStoreActions = {
   setTg: (tg: typeof WebApp) => void;
   setUserPoints: (points: number) => void;
+  setTelegramId: (id: number) => void;
   setUserTotalTimeOfUsingApp: (totalTime: number) => void;
   removeTg: () => void;
   removeUserId: () => void;
+  setUserLastCheckInDate: (date: Date) => void;
+  incrementUserPoints: (points: number) => void;
 };
 
 export const useBaseStore = create<BaseStoreState & BaseStoreActions>(
@@ -22,8 +27,10 @@ export const useBaseStore = create<BaseStoreState & BaseStoreActions>(
     tg: null,
     userId: null,
     userName: null,
+    telegramId: null,
     userPoints: null,
     userTotalTimeOfUsingApp: null,
+    userLastCheckInDate: null,
     setTg: (tg: typeof WebApp) =>
       set(() => ({
         tg: tg,
@@ -33,7 +40,12 @@ export const useBaseStore = create<BaseStoreState & BaseStoreActions>(
     removeTg: () => set({ tg: null }),
     removeUserId: () => set({ userId: null }),
     setUserPoints: (points: number) => set({ userPoints: points }),
+    incrementUserPoints: (points: number) => {
+      set((state) => ({ userPoints: state.userPoints! + points }));
+    },
+    setTelegramId: (id: number) => set({ telegramId: id }),
     setUserTotalTimeOfUsingApp: (totalTime: number) =>
       set({ userTotalTimeOfUsingApp: totalTime }),
+    setUserLastCheckInDate: (date: Date) => set({ userLastCheckInDate: date }),
   })
 );
