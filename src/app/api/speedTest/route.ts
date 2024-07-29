084955607827200b5974df954e71db06ad7543cd
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-29 16:32:45 +0300

    refactor: Update speedTest route to store user location in database

--- src/app/api/speedTest/route.ts
+++ src/app/api/speedTest/route.ts
@@ -1,22 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 import { exec } from "child_process";
 import util from "util";
-
+import { ConnectionResponseData } from "@/types";
+import prisma from "@/lib/db";
 const execPromise = util.promisify(exec);
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const { userTelegramId } = await req.json();
   try {
     const command = `fast --upload --json`;
     const { stdout, stderr } = await execPromise(command);
-    console.log("1");
     if (stderr) {
       console.error(`exec error: ${stderr}`);
       return NextResponse.json({ error: stderr }, { status: 400 });
     }
-
-    console.log("2");
-    const data = JSON.parse(stdout);
-    return NextResponse.json(data, { status: 200 });
+    const data: ConnectionResponseData = JSON.parse(stdout);
+    const timestamp = new Date().toISOString();
+    console.log(timestamp);
+    console.log(data);
+    await prisma.user.update({
+      where: { telegramId: userTelegramId },
+      data: { location: data.userLocation },
+    });
+    return NextResponse.json(
+      {
+        downloadSpeed: data.downloadSpeed,
+        uploadSpeed: data.uploadSpeed,
+        userLocation: data.userLocation,
+        userIp: data.userIp,
+        timestamp,
+      },
+      { status: 200 }
+    );
   } catch (e) {
     console.log(e);
     return NextResponse.json({ error: e }, { status: 400 });
