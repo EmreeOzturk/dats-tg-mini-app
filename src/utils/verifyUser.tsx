4b3e8566fdb72cfcb07b2eee4cbaf3f693a3aa74
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-31 14:04:29 +0300

    refactor: Remove console.log statements from verifyUser.tsx

--- src/utils/verifyUser.tsx
+++ src/utils/verifyUser.tsx
@@ -11,7 +11,6 @@ export function verifyInitData(telegramInitData: string): boolean {
   dataCheckString = dataCheckString.slice(0, -1);
 
 
-  console.log(process.env.TELEGRAM_TOKEN);
   const secret = crypto
     ?.createHmac("sha256", "WebAppData")
     ?.update(process.env.TELEGRAM_TOKEN!);
@@ -20,9 +19,6 @@ export function verifyInitData(telegramInitData: string): boolean {
     .createHmac("sha256", secret.digest())
     .update(dataCheckString)
     .digest("hex");
-
-  console.log(calculatedHash, hash);
-  console.log(calculatedHash === hash);
 
   return calculatedHash === hash;
 }
