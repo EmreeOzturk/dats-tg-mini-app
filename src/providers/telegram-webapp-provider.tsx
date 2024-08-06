86fff9d9050658af021f2b38f8d9324835dbf653
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-06 05:45:15 +0300

    refactor: Update TelegramWebAppProvider to use useRouter and useSearchParams

--- src/providers/telegram-webapp-provider.tsx
+++ src/providers/telegram-webapp-provider.tsx
@@ -2,27 +2,38 @@
 import { PropsWithChildren, useEffect } from "react"
 import { useQuery } from "@tanstack/react-query";
 import { useBaseStore } from "@/store/useBaseStore";
-
-import useTelegram from "@/hooks/useTelegram";
+import { useRouter, useSearchParams } from "next/navigation";
 import WebApp from "@twa-dev/sdk";
 import LoadingPage from "@/components/loading";
 const TelegramWebAppProvider = ({ children }: PropsWithChildren<{}>) => {
-    const { removeTelegram, initTelegram } = useTelegram();
     const setUserPoints = useBaseStore((state) => state.setUserPoints);
     const setUserTotalTimeOfUsingApp = useBaseStore((state) => state.setUserTotalTimeOfUsingApp);
     const setUserLastCheckInDate = useBaseStore((state) => state.setUserLastCheckInDate);
     const setIsFollowingTwitter = useBaseStore((state) => state.setIsFollowingTwitter);
+    const setTg = useBaseStore((state) => state.setTg);
+    const setImg = useBaseStore((state) => state.setImg);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const img = searchParams.get("img");
     useEffect(() => {
-        initTelegram();
-        return () => {
-            removeTelegram();
+        if (typeof window !== "undefined") {
+            WebApp.ready()
+            WebApp.setHeaderColor("#000055")
+            WebApp.expand()
+            WebApp.enableClosingConfirmation()
+            WebApp.MainButton.hide()
+            WebApp.BackButton.show()
+            WebApp.BackButton.onClick(() => {
+                router.back()
+            })
+            setTg(WebApp);
+            setImg(img!);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-
-    const { data, isLoading } = useQuery({
-        queryFn: async () => await fetch("/api/validateUser", {
+    const { isLoading } = useQuery({
+        queryFn: () => fetch("/api/validateUser", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -38,12 +49,7 @@ const TelegramWebAppProvider = ({ children }: PropsWithChildren<{}>) => {
     });
 
 
-    return (
-        <>
-            {
-                isLoading ? <LoadingPage /> : children
-            }
-        </>
-    )
+
+    return isLoading ? <LoadingPage /> : <>{children}</>;
 }
 export default TelegramWebAppProvider
