d3360a04df4133476f637280b0bb19172d07c76b
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-06 05:44:35 +0300

    refactor: Update Header component to use UserPhoto component and adjust styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,21 @@
 "use client"
-import Image from "next/image";
 import { useBaseStore } from "@/store/useBaseStore";
+import UserPhoto from "./home/user-photo";
 const Header = () => {
     const userName = useBaseStore((state) => state.userName);
     const userPoints = useBaseStore((state) => state.userPoints);
     return (
-        <header className='flex w-[96dvw] overflow-hidden items-center justify-between py-1 border-b px-4 rounded-2xl mt-2'>
+        <header className='flex w-[96dvw] overflow-hidden items-center justify-between pt-2 pb-3 border-b px-4 rounded-2xl'>
             <div className='flex items-center justify-between gap-2 w-full  text-sm font-bold'>
                 <div>
                     <p className="font-bold text-base">
                         {userName}
                     </p>
-                    <p className="font-bold text-base">
-                        XP: {userPoints}
-                    </p>
+                    {/* <p className="font-bold text-base">
+                        DP: {userPoints}
+                    </p> */}
                 </div>
-                <Image priority src="/mavi3.png" alt="dats project logo" width={40} height={40} className="object-cover" />
+                <UserPhoto />
             </div>
             {/* <button className="bg-black border z-10 tracking-wide border-black text-sm  w-fit text-white px-5 py-2">
                 Close App
