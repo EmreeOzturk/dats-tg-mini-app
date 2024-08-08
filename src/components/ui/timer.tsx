9c782510372c07d588bb1bcaabc853fa84246eba
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-08 20:19:03 +0300

    refactor: Update Timer component to use initial countdown value

--- src/components/ui/timer.tsx
+++ src/components/ui/timer.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import { useAnimate } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { useCountdownStore } from "@/store/useCountdownStore";
+
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
@@ -9,12 +10,13 @@ const DAY = HOUR * 24;
 
 type Units = "Day" | "Hour" | "Minute" | "Second";
 
+const INITIAL_COUNTDOWN = 6 * HOUR; // 6 hours in milliseconds
+
 const Timer = () => {
     return (
         <div className="mx-auto flex w-fit max-w-5xl flex-wrap items-center text-zinc-50 px-4 justify-center gap-x-4 text-xs md:text-sm
             bg-gradient-to-r from-sky-950/30 via-sky-600/50 to-sky-950/30 rounded-full border border-sky-950 
         ">
-            <CountdownItem unit="Day" text="days" />
             <CountdownItem unit="Hour" text="hours" />
             <CountdownItem unit="Minute" text="minutes" />
             <CountdownItem unit="Second" text="seconds" />
@@ -39,7 +41,6 @@ const CountdownItem = ({ unit, text }: { unit: Units; text: string }) => {
     );
 };
 
-
 export default Timer;
 
 const useTimer = (unit: Units) => {
@@ -52,23 +53,14 @@ const useTimer = (unit: Units) => {
 
     const timeRef = useRef(0);
 
-    const [time, setTime] = useState(
-        Math.floor(
-            (accumulatedTime + (isRunning ? Date.now() - startTime : 0)) /
-            (unit === "Day"
-                ? DAY
-                : unit === "Hour"
-                    ? HOUR
-                    : unit === "Minute"
-                        ? MINUTE
-                        : SECOND)
-        )
-    );
+    const [time, setTime] = useState(0);
 
     useEffect(() => {
+        updateInitialTime(); // Set initial time when the component mounts
+
         if (isRunning) {
             if (!intervalRef.current) {
-                intervalRef.current = setInterval(handleStopwatch, 1000);
+                intervalRef.current = setInterval(handleCountdown, 1000);
             }
         } else {
             clearInterval(intervalRef.current || undefined);
@@ -79,39 +71,56 @@ const useTimer = (unit: Units) => {
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isRunning]);
-    const handleStopwatch = async () => {
+
+    const updateInitialTime = () => {
         const now = Date.now();
         const elapsed = accumulatedTime + (isRunning ? now - startTime : 0);
+        const remainingTime = INITIAL_COUNTDOWN - elapsed;
 
-        let newTime = 0;
+        setTime(calculateTime(remainingTime, unit));
+    };
 
-        if (unit === "Day") {
-            newTime = Math.floor(elapsed / DAY);
-        } else if (unit === "Hour") {
-            newTime = Math.floor((elapsed % DAY) / HOUR);
-        } else if (unit === "Minute") {
-            newTime = Math.floor((elapsed % HOUR) / MINUTE);
-        } else {
-            newTime = Math.floor((elapsed % MINUTE) / SECOND);
-        }
+    const handleCountdown = async () => {
+        const now = Date.now();
+        const elapsed = accumulatedTime + (isRunning ? now - startTime : 0);
+        const remainingTime = INITIAL_COUNTDOWN - elapsed;
+
+        const newTime = calculateTime(remainingTime, unit);
+
 
         if (newTime !== timeRef.current) {
             // Exit animation
-            await animate(
-                ref.current,
-                { y: ["0%", "-50%"], opacity: [1, 0] },
-                { duration: 0.35 }
-            );
+            try {
+                await animate(
+                    ref.current,
+                    { y: ["0%", "-50%"], opacity: [1, 0] },
+                    { duration: 0.35 }
+                );
 
-            timeRef.current = newTime;
-            setTime(newTime);
+                timeRef.current = newTime;
+                setTime(newTime);
 
-            // Enter animation
-            await animate(
-                ref.current,
-                { y: ["50%", "0%"], opacity: [0, 1] },
-                { duration: 0.35 }
-            );
+                // Enter animation
+                await animate(
+                    ref.current,
+                    { y: ["50%", "0%"], opacity: [0, 1] },
+                    { duration: 0.35 }
+                );
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    };
+
+    const calculateTime = (remainingTime: number, unit: Units) => {
+        if (unit === "Day") {
+            return Math.floor(remainingTime / DAY);
+        } else if (unit === "Hour") {
+            return Math.floor((remainingTime % DAY) / HOUR);
+        } else if (unit === "Minute") {
+            return Math.floor((remainingTime % HOUR) / MINUTE);
+        } else {
+            return Math.floor((remainingTime % MINUTE) / SECOND);
         }
     };
 
